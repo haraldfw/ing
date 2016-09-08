9b1df4d17fce2467465cc3ef200aeae51dadd9a8
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2016-09-08 18:39:12 +0200

    some methods for sys oving 3

--- 3/sys/oving/src/main/java/src/wilhelmsen/sys/three/producer/KundeService.java
+++ 3/sys/oving/src/main/java/src/wilhelmsen/sys/three/producer/KundeService.java
@@ -2,17 +2,12 @@ package src.wilhelmsen.sys.three.producer;
 
 import src.wilhelmsen.sys.three.model.Kunde;
 
+import javax.websocket.server.PathParam;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import javax.websocket.server.PathParam;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.NotFoundException;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 /**
  * Created by Harald Floor Wilhelmsen on 29.08.2016.
@@ -37,15 +32,38 @@ public class KundeService {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Collection<Kunde> getKunder() {
-        System.out.println("get on kunder");
+        System.out.println("get kunder");
         return kunder.values();
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public void addKunde(Kunde kunde) {
-        System.out.println("post in kunder");
         kunder.put(kunde.getId(), kunde);
+    }
+
+    @DELETE
+    @Path("/oving3/kunder/{kundeId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void deleteKunde(@PathParam("kundeId") String kundeId) {
+        System.out.println("Kunde with id '" + kundeId + "' was deleted");
+        if (!kunder.containsKey(kundeId)) {
+            throw new NotFoundException("Kunde with id '" + kundeId + "' does not exist");
+        }
+        kunder.remove(kundeId);
+        System.out.println("Kunde with id '" + kundeId + "' was deleted");
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void updateKunde(Kunde kunde) {
+        String kundeId = kunde.getId();
+        Kunde localKunde = kunder.get(kundeId);
+        if (localKunde == null) {
+            throw new NotFoundException("Kunde with id '" + kundeId + "' does not exist");
+        }
+        localKunde.setNavn(kunde.getNavn());
+        System.out.println("Kunde with kundeid '" + kundeId + "' updated");
     }
 
     // TODO: 01.09.2016
